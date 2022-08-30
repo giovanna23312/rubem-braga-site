5e9e2af68befe984b855e683f362673d1f49d84e
giovanna23312 <111871714+giovanna23312@users.noreply.github.com>
2022-08-30 11:53:45 -0300

    primeiro JAVA

--- js/code.js
+++ js/code.js
@@ -1,2 +1,38 @@
 //Códigos para o Hot site Rubem Braga
 
+
+    //cria um novo objeto da classe date
+
+const tempo = new Date()
+const txtData = tempo.getFullYear() + ' - hora: ' + tempo.getHours() + ' minuto: ' + tempo.getMinutes()
+
+//elementos DOM
+const spanData = document.getElementById('data')
+const btFechar = document.getElementById('btFechar')
+const divModal = document.getElementById('divModal')
+const btHamburger = document.getElementById('btHamburger')
+const navPhone = document.getElementById('navPhone')
+const FecharPhone = document.getElementById('FecharPhone')
+
+//insere txtData dentro do id "data"
+
+spanData.innerText = txtData
+
+//Ações do usuario (eventos)
+
+//Ao clicar no id "btFechar"
+
+btFechar.addEventListener('click', function(){
+     //oculta o divModal- CSS display-none (por javascript)
+
+     divModal.style.display = 'none'
+
+})
+  
+btHamburger.addEventListener('click', function(){
+    navPhone.style.display = 'block'
+})
+
+FecharPhone.addEventListener('click', function(){
+    navPhone.style.display = 'none'
+})
